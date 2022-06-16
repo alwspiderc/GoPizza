9f56d8b70ba53e9c05b174fd2e91768b87421463
Ester Costa de Souza <ester.souza@sinart.com.br>
2022-06-16 09:34:09 -0300

    StatusBar, Component Input

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Input } from "@components/Input";
 import { Container } from "./styles";
 
 export function SignIn() {
-    return (
-        <Container>
-    
-        </Container>
-    );
+  return (
+    <Container>
+      <Input
+        placeholder="E-mail"
+        type="secondary"
+        autoCorrect={false}
+        autoCapitalize="none"
+      />
+      <Input
+        placeholder="Senha"
+        type="secondary"
+        secureTextEntry
+      />
+    </Container>
+  );
 }
